9fda85d3571bccee85dc882edf996eaa31f289e5
ralastri <raffaello.lastrico@yahoo.de>
2022-01-11 12:54:34 +0100

    HOBO Zeitreihe kann jetzt eingelesen werden und mit dygraph angeschaut werden!

--- 01_test_skript.R
+++ 01_test_skript.R
@@ -6,7 +6,9 @@ hobo <- read.csv("~/02 UNI Master Hydro/1. Semester/04 Data Collection Storage a
 
 library(dygraphs, tidyverse, dplyr)
 
-tempseries <- hobo %>% dplyr::select(dttm = Date.Time..GMT.01.00, temp = Temp..Â.C..LGR.S.N..10347366..SEN.S.N..10347366.)
+tempseries <- hobo %>% dplyr::select(dttm = Date.Time..GMT.01.00, light = Intensity..Lux..LGR.S.N..10347366..SEN.S.N..10347366., temp = Temp..Â.C..LGR.S.N..10347366..SEN.S.N..10347366.)
 tempseries$dttm <- as.POSIXct(tempseries$dttm, format = "%d/%m/%Y %H:%M:%S")
 
-dygraph(tempseries)
+dygraph(xts::xts(tempseries[,-1], order.by = tempseries$dttm)) %>% 
+  dyRangeSelector() %>% 
+  dySeries("light", axis = 'y2')
